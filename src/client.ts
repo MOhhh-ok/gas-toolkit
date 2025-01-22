56ddac9a9c71ad34a01c5052de066b2861c62d23
MOhhh <64732359+MOhhh-ok@users.noreply.github.com>
2025-01-22 23:17:31 +0900

    -

--- src/client.ts
+++ src/client.ts
@@ -1,13 +1,28 @@
-declare const window: any;
+declare global {
+  interface Window {
+    google?: { script: { run: any } };
+  }
+}
+
+declare const window: Window;
 
 export function googleScriptRun<Arg extends unknown, Result>(
   functionName: string,
-  arg: Arg
+  arg: Arg,
+  ops?: {
+    mock?: (arg: Arg) => Promise<Result>;
+  }
 ): Promise<Result> {
-  return new Promise((resolve, reject) => {
-    window.google.script.run
-      .withSuccessHandler((result: Result) => resolve(result))
-      .withFailureHandler((error: unknown) => reject(error))
-      [functionName](arg);
-  });
+  if (window.google) {
+    return new Promise((resolve, reject) => {
+      window.google?.script.run
+        .withSuccessHandler((result: Result) => resolve(result))
+        .withFailureHandler((error: unknown) => reject(error))
+        [functionName](arg);
+    });
+  } else if (ops?.mock) {
+    return ops.mock(arg);
+  } else {
+    throw new Error('Google Script Run is not available');
+  }
 }
